2505c49dfc09b5ca8f75c2167d717483959da3a3
Max Kreminski <maxkreminski@gmail.com>
2025-01-15 18:34:35 -0800

    Add Python script used to precompute links for prompting data
    Need to standardize and document input file format pretty soon I think

---
+++ compute_links.py
@@ -0,0 +1,27 @@
+from sentence_transformers import SentenceTransformer, util
+import json
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def compute_links(moves):
+	embs = [model.encode(move) for move in moves]
+	links = {}
+	for i in range(len(moves)):
+		links[i] = {}
+		for j in range(i):
+			links[i][j] = util.cos_sim([embs[i]], [embs[j]])[0].item()
+	return links
+
+def add_links_to_file(fpath):
+	with open(fpath, "r+") as file:
+		data = json.load(file)
+		for ep_id, episode in data.items():
+			print(ep_id)
+			data[ep_id] = {
+				"moves": episode,
+				"links": compute_links([move["text"] for move in episode])
+			}
+		file.seek(0)
+		json.dump(data, file)
+
+add_links_to_file("imggen_50.json")
